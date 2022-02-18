800a5ee019e0b99cdf67898386d9c18259de19d6
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-18 18:32:36 -0300

    Essa merda aqui

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>
+        Boa Noite!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  texto:{
+    color:'#fff',
+    fontSize:30
+  }
+  
 });
